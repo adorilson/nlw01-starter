c2ef5f178873f0a91d6da939084e1b6556324fb5
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2020-06-04 22:08:09 -0300

    Criacao de partes de templates em partials

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,6 @@
+console.log("carregou index.js...");
+
+
 const buttonSearch = document.querySelector("#page-home main a");
 const modal = document.querySelector("#modal");
 const close = document.querySelector("#modal .header");
