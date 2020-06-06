ec9ecd7e64f7acaa95b8c8366db4c2812dd1ac9f
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2020-06-05 22:24:29 -0300

    Cadastro de pontos de coleta a partir do formulario

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const db = require("./database/db")
 
 server.use(express.static("public"));
 
+server.use(express.urlencoded({extended: true}))
+
 const  nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: server,
@@ -19,6 +21,45 @@ server.get("/", (req, res) =>{
 
 server.get("/create-point", (req, res) =>{
     return res.render("create-point.html");
+})
+
+server.post("/savepoint", (req, res) => {
+
+    const query = `
+            INSERT INTO places(
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            )VALUES(?, ?, ?, ?, ?, ?, ?)
+    `
+
+    const values =  [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+    ]
+
+    function afterInsertData(err){
+        if (err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.send("ok")
+    }
+
+    db.run(query, values, afterInsertData)
+    
 })
 
 server.get("/search", (req, res) =>{
