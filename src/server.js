6305a2137e1cb6af02965f7b7f0566b99a4d52d7
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2020-06-05 23:04:53 -0300

    Exibe cadastro com sucesso e de erro ao cadastrar ponto de coleta

--- src/server.js
+++ src/server.js
@@ -49,13 +49,14 @@ server.post("/savepoint", (req, res) => {
 
     function afterInsertData(err){
         if (err){
-            return console.log(err)
+            console.log(err)
+            return res.send("Erro no cadastro.");
         }
 
         console.log("Cadastrado com sucesso")
         console.log(this)
 
-        return res.send("ok")
+        return res.render("create-point.html", {saved: true});
     }
 
     db.run(query, values, afterInsertData)
